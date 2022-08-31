0db708affd567d30e8443bc35ffe9fe9ca12c2aa
dalarin <dalarin@avito.ru>
2022-08-31 14:50:23 +0300

    init commit

---
+++ cmd/merge_two_mass/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []int{1, 2, 4, 7}
+	b := []int{3, 4, 7, 15, 32}
+	fmt.Println(mergeMass(a, b))
+
+}
+
+func mergeMass(a, b []int) []int {
+	resultLen := len(a) + len(b)
+	result := make([]int, 0, resultLen)
+	countA := 0
+	countB := 0
+
+	for i := 0; i < resultLen; i++ {
+		if countA >= len(a) {
+			result = append(result, b[countB:]...)
+			break
+		}
+		if countB >= len(b) {
+			result = append(result, a[countA:]...)
+			break
+		}
+
+		if a[countA] > b[countB] {
+			result = append(result, b[countB])
+			countB++
+		} else {
+			result = append(result, a[countA])
+			countA++
+		}
+	}
+	return result
+}
